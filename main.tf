85259eea39f02d633566ff62b96dfc028ce7a47f
Matt Spahr <matt.spahr@matt.spahr-M4MGYYK3WC>
2024-09-11 15:39:09 -0500

    changed name

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ data "hcp_packer_artifact" "hello-world" {
   region       = "us-east-2"
 }
 
-resource "aws_instance" "python_backend" {
+resource "aws_instance" "hello_world" {
   ami             = data.hcp_packer_artifact.hello-world.external_identifier
   instance_type   = "t2.micro"
   security_groups = [aws_security_group.web_sg.name]
